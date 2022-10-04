c02c6d8c9d8e1f15de5c6eb13a3148d8f4008307
milon27 <mdjahidulislammilon@gmail.com>
2022-10-04 13:04:37 +0600

    update gitignore

--- dist/utils/Define.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Define = {
-    TOKEN: "token"
-};
-exports.default = Define;
-//# sourceMappingURL=Define.js.map
